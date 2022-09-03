defcfbb94a2955a2a376e2212d14d66c1d4dae21
Jesus Escobedo <jesuseedc@outlook.com>
2022-09-03 16:01:10 -0700

    edit tarea-01/problemas.rkt

--- tarea-01/problemas.rkt
+++ tarea-01/problemas.rkt
@@ -2,4 +2,18 @@
 
 ;; Escribe aquí tus soluciones
 
+(define countdown
+  (lambda (n)
+    (if (= n 0)
+        '(0)
+        (cons n (countdown (- n 1))))))
+
+
+(define insertL
+  (lambda (sym1 sym2 lst)
+    (cond
+      [(null? lst) '()]
+      [(eqv? (car lst) sym1) (cons sym2 (cons sym1 (insertL sym1 sym2 (cdr lst))))]
+      [else (cons (car lst) (insertL sym1 sym2 (cdr lst)))])))
+
 (provide (all-defined-out))
